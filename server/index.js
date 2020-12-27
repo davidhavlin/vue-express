72a1402651c6a191810f559b75eb8d078d9d3600
davidhavlin <davidhavlin@gmail.com>
2020-12-27 19:12:32 +0100

    Hlavne subory vytvorene

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+// middlewares
+// app.use(bodyParser.json()); // stara moznost teraz mozem pouzivat rovno express
+app.use(express.json());
+app.use(cors()); // na cors blbosti z chromu
+
+const port = process.env.PORT || 5000;
+
+const posts = require("./routes/api/posts");
+app.use("/api/posts", posts);
+
+app.listen(port, () => console.log(`server started on port: ${port}`));
---
+++ server/routes/api/posts.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const mongodb = require("mongodb");
+
+const router = express.Router();
+
+// GET POSTS
+router.get("/", async (req, res) => {
+	const posts = await loadPostsCollection();
+	res.send(await posts.find({}).toArray());
+});
+
+// ADD POST
+router.post("/", async (req, res) => {
+	const posts = await loadPostsCollection();
+	await posts.insertOne({
+		title: req.body.title,
+		text: req.body.text,
+		created_at: new Date(),
+	});
+	res.status(201).send();
+});
+
+// DELETE POST
+router.delete("/:id", async (req, res) => {
+	const posts = await loadPostsCollection();
+	await posts.deleteOne({ _id: new mongodb.ObjectID(req.params.id) });
+	res.status(200).send();
+});
+
+// UPDATE POST
+router.put("/:id", async (req, res) => {
+	const posts = await loadPostsCollection();
+	await posts.updateOne(
+		{ _id: new mongodb.ObjectID(req.params.id) },
+		{
+			$set: {
+				title: req.body.title,
+				text: req.body.text,
+				updated_at: new Date(),
+			},
+		}
+	);
+	res.status(200).send();
+});
+
+async function loadPostsCollection() {
+	const client = await mongodb.MongoClient.connect(
+		`mongodb+srv://david:kazafat1234@vue-express.shjpn.mongodb.net/vue-express?retryWrites=true&w=majority`,
+		{
+			useNewUrlParser: true,
+		}
+	);
+
+	return client.db("vue-express").collection("posts");
+}
+
+module.exports = router;
